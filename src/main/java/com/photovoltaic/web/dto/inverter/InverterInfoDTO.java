df12948f8e96113796a64c4bbf08335f5f5ebd19
happyjun190 <happyjun190@gmail.com>
2017-04-17 18:05:44 +0800

    测试样例添加

---
+++ src/main/java/com/photovoltaic/web/dto/inverter/InverterInfoDTO.java
@@ -0,0 +1,9 @@
+package com.photovoltaic.web.dto.inverter;
+
+/**
+ * Created by wushenjun on 2017/4/17.
+ */
+public class InverterInfoDTO {
+    private String inverterId;//逆变器id
+    private String inverterName;//逆变器名称
+}
