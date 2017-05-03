550b11f88266d4bef42beefcd50963937a07f60d
happyjun190 <happyjun190@gmail.com>
2017-05-03 22:19:23 +0800

    app login controller

---
+++ src/main/java/com/photovoltaic/web/www/BaseController.java
@@ -0,0 +1,12 @@
+package com.photovoltaic.web.www;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by wushenjun on 2017/3/6.
+ * 基类controller,写一些通用的设置
+ */
+public class BaseController {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+}
